cf5435beeef2d693b2ce5c9c817779d957d9d04f
Den-Gor <dengor.gor2017@yandex.ru>
2026-09-17 22:10:09 +0700

    Фикс бага с бесконечным сердцебиением
    добавил проверку IsTrueCrit в OnMobStateChanged

--- Content.Server/_Sunrise/Heartbeat/Systems/HeartbeatSystem.Crit.cs
+++ Content.Server/_Sunrise/Heartbeat/Systems/HeartbeatSystem.Crit.cs
@@ -3,12 +3,15 @@ using Content.Shared.Damage;
 using Content.Shared.Damage.Components;
 using Content.Shared.Damage.Systems;
 using Content.Shared.Mobs;
+using Content.Shared.Mobs.Components;
+using Content.Shared.Mobs.Systems;
 
 namespace Content.Server._Sunrise.Heartbeat.Systems;
 
 public sealed partial class HeartbeatSystem
 {
     [Dependency] private DamageableSystem _damageable = default!;
+    [Dependency] private MobThresholdSystem _mobThreshold = default!;
 
     // Минимальное и максимальное время между ударами сердца
     private const float MinimumCooldown = 0.5f;
@@ -16,7 +19,7 @@ public sealed partial class HeartbeatSystem
 
     private void OnMobStateChanged(Entity<CritHeartbeatComponent> ent, ref MobStateChangedEvent args)
     {
-        if (args.NewMobState != MobState.Critical)
+        if (args.NewMobState != MobState.Critical || !IsTrueCrit(ent))
         {
             RemComp<ActiveHeartbeatComponent>(ent);
             return;
@@ -59,6 +62,24 @@ public sealed partial class HeartbeatSystem
         ent.Comp.NextHeartbeatCooldown = TimeSpan.FromSeconds(cooldownSeconds);
 
         return true;
+    }
+
+    /// <summary>
+    /// Проверяем персонажа действительно ли он должен сейчас быть в крите или нет, возрващает true если не находит компаненты / он в крите, false если он не должен быть в крите по хп.
+    /// </summary>
+    /// <param name="ent"></param>
+    /// <param name="damageable"></param>
+    /// <returns></returns>
+    private bool IsTrueCrit(Entity<CritHeartbeatComponent> ent,DamageableComponent? damageable = null)
+    {
+        if (!Resolve(ent.Owner, ref damageable))
+            return true;
+
+        var totalDamage = _damageable.GetTotalDamage((ent.Owner, damageable));
+
+        return !_mobThreshold.TryGetThresholdForState(ent.Owner, MobState.Critical, out var critThreshold)
+               || totalDamage >= critThreshold;
+
     }
 
 }
